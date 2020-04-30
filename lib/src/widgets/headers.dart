8e8d06fd31fce2c38f21e371cd99075a368fcbe5
tinnit1 <yosoy1.,isa>
2020-04-30 11:29:47 -0500

    headers - custom paint

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xFF615AAB),
+    );
+  }
+}
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+          color: Color(0xFF615AAB),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50))),
+    );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propertis of pen
+    paint.color = Color(0xFF615AAB);
+    // stroke hace la linea
+    // fill hace la linea y pinta su contenido
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = new Path();
+
+    // draw with pen and properties
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propertis of pen
+    paint.color = Color(0xFF615AAB);
+    // stroke hace la linea
+    // fill hace la linea y pinta su contenido
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = new Path();
+
+    // draw with pen and properties
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    // canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propertis of pen
+    paint.color = Color(0xFF615AAB);
+    // stroke hace la linea
+    // fill hace la linea y pinta su contenido
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = new Path();
+
+    // draw with pen and properties
+    path.moveTo(0, 0); // lleva el pointer a dicha position sin pintar
+    path.lineTo(0, size.height * 0.3);
+    path.lineTo(size.width * 0.5, size.height * 0.4);
+    path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+    // canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propertis of pen
+    paint.color = Color(0xFF615AAB);
+    // stroke hace la linea
+    // fill hace la linea y pinta su contenido
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = new Path();
+
+    // draw with pen and properties
+    path.moveTo(0, 0); // lleva el pointer a dicha position sin pintar
+    path.lineTo(0, size.height * 0.25);
+    // x1, y1 pendiente x2, y2 point final
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.5, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    // canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propertis of pen
+    paint.color = Color(0xFF615AAB);
+    // stroke hace la linea
+    // fill hace la linea y pinta su contenido
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = new Path();
+
+    // draw with pen and properties
+    path.moveTo(0, 0); // lleva el pointer a dicha position sin pintar
+    path.lineTo(0, size.height * 0.25);
+    // x1, y1 pendiente x2, y2 point final
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.35, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.15, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    // canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+
